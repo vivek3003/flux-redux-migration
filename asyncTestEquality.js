43f12c9f6f8e7a82006100c6b3f4255520c361d6
Vivek Kaarthek <vkaarthek@gmail.com>
2016-02-20 19:11:35 +0530

    Update AsyncTestEquality

--- asyncTestEquality.js
+++ asyncTestEquality.js
@@ -1,8 +1,14 @@
+/*
+    This file tests asynchronously 2 params for equality.
+    Can be used to validate your get APIs in the flux store.
+*/
 import _ from "lodash";
 export default function asyncTestEquality(param1, param2, testName){
-    if(_.isEqual(param1, param2)){
-        console.log('LOG:TEST:SUCCESS:', testName);
-    }else{
-        console.log('LOG:TEST:FAIL:', testName, param1, param2);
-    }
+    setTimeout(function(){
+        if(_.isEqual(param1, param2)){
+            console.log('LOG:TEST:SUCCESS:', testName);
+        }else{
+            console.log('LOG:TEST:FAIL:', testName, param1, param2);
+        }
+    }, 0);
 }
